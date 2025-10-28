d759868df28c965458c72c88d2641c83206703d1
siqueiradelimarafaella-boop <siqueiradelimarafaella@gmail.com>
2025-10-27 22:02:33 -0300

    first commit

---
+++ Fundamentos/tipos_de_dados.py
@@ -0,0 +1,15 @@
+print(1+10) # testar int
+print (1.5+3.4) # testar float
+print(True) # testar bool (boleano)
+print("python") # testar str (string)
+
+# Como chamar 
+int()
+float()
+bool()
+str()
+
+# Modo interativo
+# Escrever python, dentro do terminal para fazer testes diretos 
+# Funcao dir() - Lista de metodos utilizados pelo objeto
+# Funcao help() - Chama o sistema de ajuda integrado e mostra a documentacao de metodos 
---
+++ Fundamentos/variaveis_constantes.py
@@ -0,0 +1,17 @@
+nome = "Guilherme"
+idade = 28
+
+print(nome, idade)
+
+limite_saque = 1000
+# utilizar nomes sugestivos para facilitar a leitura e melhor a manutencao
+# snake case 
+
+ESTADOS =   ["RJ", "SP", "MG"]
+print(ESTADOS)
+# convencionou variaveis escritas em letra maiuscula como constantes
+
+nome2, idade2 = ("Giovana", 26)
+print (nome2, idade2)
+
+# Da pra declarar mais de uma variavel na mesma linha, utilizando a virgula.
