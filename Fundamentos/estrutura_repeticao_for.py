c4a107f741a9a3fb8584e4e1cdd4373d3802d93a
siqueiradelimarafaella-boop <siqueiradelimarafaella@gmail.com>
2025-11-03 09:03:54 -0300

    Ultima atualização

--- Fundamentos/estrutura_repeticao_for.py
+++ Fundamentos/estrutura_repeticao_for.py
@@ -1,13 +1,13 @@
-texto = input("Informe um texto: ")
-VOGAIS = "AEIOU"
+# texto = input("Informe um texto: ")
+# VOGAIS = "AEIOU"
 
 
-# Exemplo utilizando um iterável
-for letra in texto:
-    if letra.upper() in VOGAIS:
-        print(letra, end="")
-else:
-    print()  # adiciona uma quebra de linha
+# # Exemplo utilizando um iterável
+# for letra in texto:
+#     if letra.upper() in VOGAIS:
+#         print(letra, end="")
+# else:
+#     print()  # adiciona uma quebra de linha
 
 
 # Exemplo utilizando a função built-in range
